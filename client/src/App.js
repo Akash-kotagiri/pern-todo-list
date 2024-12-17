0e59a66c8458e3d5e7f062c8e93961d5dbc2b5be
Kotagiri Akash <kotagiriakash5@gmail.com>
2024-12-17 19:55:09 +0530

    Initial commit of PERN stack Todo app

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+//components
+import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodos";
+
+
+
+
+
+const App = () => {
+  return (
+    <>
+    <div className="container">
+      <InputTodo/>
+      <ListTodos/>
+    </div>
+    </>
+  );
+}
+
+export default App;
